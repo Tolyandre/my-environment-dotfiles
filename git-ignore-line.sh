54b9106413e3bc1cd26e406a8f1af4120bdb1bfc
Anatoly Buranov <tolyandre@yandex.ru>
2020-08-17 00:57:17 +0400

    ignore-line: ignore regex from .gitattributes

--- git-ignore-line.sh
+++ git-ignore-line.sh
@@ -22,9 +22,9 @@ then
 
    git show HEAD:$clean > $tmpfile
 
-   regex='ConWnd X'
-
-   diff --unified=0 <(grep -vE "$regex" $tmpfile) <(grep -vE "$regex" $clean) |\
+   diff --unified=0 \
+   <(grep -vE -f <(git check-attr -a $clean | awk '$2 ~ /ignore-regex\d*/ { print $3 }') -- $tmpfile) \
+   <(grep -vE -f <(git check-attr -a $clean | awk '$2 ~ /ignore-regex\d*/ { print $3 }') -- $clean) |\
       patch $tmpfile -o - --quiet --batch
 
 
@@ -34,5 +34,5 @@ then
    #     patch $clean -o - --quiet --batch
 fi
 
-echo "$pwd I am git-ignore-line" $@
-echo $((1 + RANDOM % 10))
+# echo "$pwd I am git-ignore-line" $@
+# echo $((1 + RANDOM % 10))
